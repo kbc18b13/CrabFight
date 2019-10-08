637e0fddfe5b3a5f64f56acf262db50a78585f8e
kbc18b13 <kbc18b13@stu.kawahara.ac.jp>
2019-10-08 17:00:49 +0900

    カニprefab

---
+++ CrabFight/Assets/Script/KaniGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaniGenerator : MonoBehaviour
+{
+    private static int KaniKazu = 2;
+
+    public static void SetKaniKazu(int kazu)
+    {
+        KaniKazu = kazu;
+    }
+
+    void Awake()
+    {
+        
+    }
+}
