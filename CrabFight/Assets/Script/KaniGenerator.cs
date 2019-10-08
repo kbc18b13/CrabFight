b02f2dd990b6d103983edb6032557ee974d6293f
kbc18b13 <kbc18b13@stu.kawahara.ac.jp>
2019-10-08 17:42:55 +0900

    カニ生成

--- CrabFight/Assets/Script/KaniGenerator.cs
+++ CrabFight/Assets/Script/KaniGenerator.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KaniGenerator : MonoBehaviour
-{
+public class KaniGenerator : MonoBehaviour {
     private static int KaniKazu = 2;
 
     public static void SetKaniKazu(int kazu)
@@ -13,6 +12,26 @@ public class KaniGenerator : MonoBehaviour
 
     void Awake()
     {
-        
+        Vector3 pos = new Vector3(0, 2.5f, 5.0f);
+        Quaternion kakudo = new Quaternion();
+        if (KaniKazu > 1)
+        {
+            kakudo = Quaternion.Euler(0, 360.0f / KaniKazu - 1, 0);
+        }
+        for (int i = 0; i < KaniKazu; i++)
+        {
+            GameObject kani = (GameObject)Resources.Load("kani Variant");
+
+            pos = kakudo * pos;
+
+            Quaternion rot = new Quaternion();
+            Vector3 vec = -pos;
+            vec.y = 0;
+            rot.SetLookRotation(vec);
+
+            Instantiate(kani, pos, rot);
+        }
+
+        Destroy(gameObject);
     }
 }
