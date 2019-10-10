a17ca49cb5a135ac0c6c405500f0fd6301accba4
kbc18b16 <kbc18b16@stu.kawahara.ac.jp>
2019-10-10 09:48:44 +0900

    Atyousei

--- CrabFight/Assets/Script/Result.cs
+++ CrabFight/Assets/Script/Result.cs
@@ -28,21 +28,19 @@ public class Result : MonoBehaviour
         players = GameObject.FindObjectsOfType<PlayerCrab>();
 
         //テスト用、適当にランキング。
-        First = 3;
-        Second = 1;
-        Third = 2;
+        
         //蟹を表彰台にセッティング。
         //一位。
         Vector3 position = new Vector3(0.0f, 3.2f, 0.0f);
         GameObject Kani1 = (GameObject)Resources.Load("kani Variant");
         Kani1 = Instantiate(Kani1, position, Quaternion.identity);
-        Kani1.GetComponent<PlayerCrab>().SetPadNum(First - 1);
+        Kani1.GetComponent<PlayerCrab>().SetPadNum(First);
 
         //二位。
         position = new Vector3(2.0f, 1.5f, 0.0f);
         GameObject Kani2 = (GameObject)Resources.Load("kani Variant");
         Kani2 = Instantiate(Kani2, position, Quaternion.identity);
-        Kani2.GetComponent<PlayerCrab>().SetPadNum(Second - 1);
+        Kani2.GetComponent<PlayerCrab>().SetPadNum(Second);
 
         //三位。
         if (MaxPlayerCount >= 3)
@@ -50,12 +48,15 @@ public class Result : MonoBehaviour
             position = new Vector3(-2.0f, 1.5f, 0.0f);
             GameObject Kani3 = (GameObject)Resources.Load("kani Variant");
             Kani3 = Instantiate(Kani3, position, Quaternion.identity);
-            Kani3.GetComponent<PlayerCrab>().SetPadNum(Third - 1);
+            Kani3.GetComponent<PlayerCrab>().SetPadNum(Third);
 
             //四位。
             if (MaxPlayerCount == 4)
             {
-
+                position = new Vector3(0.0f, 0.0f, 2.0f);
+                GameObject Kani4 = (GameObject)Resources.Load("kani Variant");
+                Kani4 = Instantiate(Kani3, position, Quaternion.identity);
+                Kani4.GetComponent<PlayerCrab>().SetPadNum(Fourth);
 
             }
 
