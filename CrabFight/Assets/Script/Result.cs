a3283e55c7a765c60b5f8169f0cf610b0cd70ff5
kbc18b16 <kbc18b16@stu.kawahara.ac.jp>
2019-10-10 11:45:17 +0900

    R 向き反転

--- CrabFight/Assets/Script/Result.cs
+++ CrabFight/Assets/Script/Result.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Result : MonoBehaviour
 {
     public int MaxPlayerCount;      //プレイヤーの最大プレイ人数。
@@ -27,19 +27,21 @@ public class Result : MonoBehaviour
         players = new PlayerCrab[MaxPlayerCount]; //初期化。
         players = GameObject.FindObjectsOfType<PlayerCrab>();
 
-        //テスト用、適当にランキング。
-        
+        //回転用、反転させてＮｅｗｇｏする
+
+        Quaternion rot = new Quaternion();
+        rot = Quaternion.Euler(0, 180.0f, 0);
         //蟹を表彰台にセッティング。
         //一位。
         Vector3 position = new Vector3(0.0f, 3.2f, 0.0f);
         GameObject Kani1 = (GameObject)Resources.Load("kani Variant");
-        Kani1 = Instantiate(Kani1, position, Quaternion.identity);
+        Kani1 = Instantiate(Kani1, position, rot);
         Kani1.GetComponent<PlayerCrab>().SetPadNum(First);
 
         //二位。
         position = new Vector3(2.0f, 1.5f, 0.0f);
         GameObject Kani2 = (GameObject)Resources.Load("kani Variant");
-        Kani2 = Instantiate(Kani2, position, Quaternion.identity);
+        Kani2 = Instantiate(Kani2, position, rot);
         Kani2.GetComponent<PlayerCrab>().SetPadNum(Second);
 
         //三位。
@@ -47,15 +49,15 @@ public class Result : MonoBehaviour
         {
             position = new Vector3(-2.0f, 1.5f, 0.0f);
             GameObject Kani3 = (GameObject)Resources.Load("kani Variant");
-            Kani3 = Instantiate(Kani3, position, Quaternion.identity);
+            Kani3 = Instantiate(Kani3, position, rot);
             Kani3.GetComponent<PlayerCrab>().SetPadNum(Third);
 
             //四位。
             if (MaxPlayerCount == 4)
             {
-                position = new Vector3(0.0f, 0.0f, 2.0f);
+                position = new Vector3(0.0f, 0.0f, -2.0f);
                 GameObject Kani4 = (GameObject)Resources.Load("kani Variant");
-                Kani4 = Instantiate(Kani3, position, Quaternion.identity);
+                Kani4 = Instantiate(Kani3, position, rot);
                 Kani4.GetComponent<PlayerCrab>().SetPadNum(Fourth);
 
             }
@@ -68,8 +70,11 @@ public class Result : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
 
+        if (Input.GetButtonDown("B_" + First))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
 
 
     }
